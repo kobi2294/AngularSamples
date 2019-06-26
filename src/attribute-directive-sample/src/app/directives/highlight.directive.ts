5ee1efa0efe7e25e00ef1bec74d8e136ca0176c2
Kobi Hari <kobi2294@yahoo.com>
2019-06-27 01:44:38 +0300

    Modified the directive sample so that it accepts inputs

--- src/attribute-directive-sample/src/app/directives/highlight.directive.ts
+++ src/attribute-directive-sample/src/app/directives/highlight.directive.ts
@@ -4,13 +4,15 @@ import { Directive, ElementRef, Renderer2, OnInit, Input } from '@angular/core';
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit{
+  @Input() color: string;
+
   constructor(
     private renderer: Renderer2, 
     private elem: ElementRef) {}
 
   ngOnInit(): void {
     this.renderer.setStyle(this.elem.nativeElement, 
-      'background-color', 'yellow');
+      'background-color', this.color);
   }
 }
 
