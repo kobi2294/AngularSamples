b1d4ca49dc369ab8b81df9165b39a39e2e14fa89
Kobi Hari <kobi2294@yahoo.com>
2019-06-27 02:36:58 +0300

    Added output events to the custom attribute directive

--- src/attribute-directive-sample/src/app/app.component.ts
+++ src/attribute-directive-sample/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'attribute-directive-sample';
+
+  onHighlightTrigger(color: string) {
+    console.log('highlight was triggered in color ' + color);
+  }
 }
--- src/attribute-directive-sample/src/app/directives/highlight.directive.ts
+++ src/attribute-directive-sample/src/app/directives/highlight.directive.ts
@@ -1,17 +1,24 @@
 import { Directive, Input, HostListener, HostBinding, Output, EventEmitter } from '@angular/core';
 
-
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
   @Input('appHighlight') color: string;
 
+  @Output() triggered = new EventEmitter<string>();
+
   @HostBinding('style.background-color') myBackgroundColor: string;
+
+  private wasTriggered = false;
 
   @HostListener('mouseenter') onMouseOver() {
     let color = this.color || 'yellow';
     this.myBackgroundColor = color;
+    if (!this.wasTriggered) {
+      this.wasTriggered = true;
+      this.triggered.emit(color);
+    }
   }
 
   @HostListener('mouseleave') onMouseOut() {
