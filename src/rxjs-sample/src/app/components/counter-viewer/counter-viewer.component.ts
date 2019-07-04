faa5ab4a0b00dfaf46633e5ad8901d92214b44b6
Kobi Hari <kobi2294@yahoo.com>
2019-07-04 16:57:39 +0300

    using async pipe

--- src/rxjs-sample/src/app/components/counter-viewer/counter-viewer.component.ts
+++ src/rxjs-sample/src/app/components/counter-viewer/counter-viewer.component.ts
@@ -1,29 +1,23 @@
 import { CounterService } from './../../services/counter.service';
-import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { ChangeDetectionStrategy } from '@angular/compiler/src/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter-viewer',
   templateUrl: './counter-viewer.component.html',
   styleUrls: ['./counter-viewer.component.css'], 
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CounterViewerComponent implements OnInit, OnDestroy {
-  counter: number;
-  private sub: Subscription;
+export class CounterViewerComponent implements OnInit {
+  counter$: Observable<number>;
 
   constructor(private counterService: CounterService) {}
 
   ngOnInit() {
-    this.sub = this.counterService.getCounter().subscribe(val => {
-      this.counter = val;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
+    this.counter$ = this.counterService.getCounter();
   }
 }
+
+
 
 
