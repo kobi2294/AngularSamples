9b9e335069817da2e72ded49770067015567d9ce
Kobi Hari <kobi2294@yahoo.com>
2019-07-06 23:15:35 +0300

    colors-rxjs-operators after 2.3.3

--- src/colors-rxjs-operators/src/app/services/colors.service.ts
+++ src/colors-rxjs-operators/src/app/services/colors.service.ts
@@ -7,4 +7,19 @@ import { ColorModel } from '../models/colors.model';
 })
 export class ColorsService {
   constructor() { }
+
+  delay(millis: number): Promise<void> {
+    return new Promise((resolve, reject) => setTimeout(resolve, millis));
+  }
+
+  async searchColors(filter: string): Promise<ColorModel[]> {
+    filter = filter.toLowerCase();
+    const entries = Object.entries(ALLCOLORS);
+    const res = entries
+      .map(entry => {return {displayName: entry[1], code: entry[0]}})
+      .filter(color => color.displayName.toLowerCase().includes(filter))
+      
+    await this.delay(3000);
+    return res;
+  }
 }
