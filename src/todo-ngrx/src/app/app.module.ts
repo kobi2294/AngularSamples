9a393caffe14cf77227c7cd11d4d1db58d8e1439
Kobi Hari <kobi2294@yahoo.com>
2019-07-07 03:39:00 +0300

    todo-ngrx after 3.3.1 material installation

--- src/todo-ngrx/src/app/app.module.ts
+++ src/todo-ngrx/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { AppMaterialModule } from './app-material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    BrowserAnimationsModule, 
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
