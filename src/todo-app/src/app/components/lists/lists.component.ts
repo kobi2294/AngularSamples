c6439a293e4ca2cf77238f48da507f3cb2c11f71
Kobi Hari <kobi2294@yahoo.com>
2020-03-07 19:37:10 +0200

    Step 5 - List view design

--- src/todo-app/src/app/components/lists/lists.component.ts
+++ src/todo-app/src/app/components/lists/lists.component.ts
@@ -1,4 +1,8 @@
+import { StateService } from './../../core/services/state.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { TodoList } from 'src/app/core/models/todo-list.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lists',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  lists$: Observable<TodoList[]>;
 
-  constructor() { }
+  constructor(
+    private state: StateService, 
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.lists$ = this.state.getAllLists();
+  }
+
+  navigateToList(id: number) {
+    this.router.navigate(['lists', id]);
+  }
+
+  navigateToNew() {
+    this.router.navigate(['lists', -1, 'edit']);
   }
 
 }
