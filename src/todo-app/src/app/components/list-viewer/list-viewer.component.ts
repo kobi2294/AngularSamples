0f0c2afd6d09286a3939d09eb2413fa0126f8519
Kobi Hari <kobi2294@yahoo.com>
2020-03-09 11:54:11 +0200

    Step 6 - Creating VM Observables

--- src/todo-app/src/app/components/list-viewer/list-viewer.component.ts
+++ src/todo-app/src/app/components/list-viewer/list-viewer.component.ts
@@ -1,4 +1,10 @@
+import { StateService } from './../../core/services/state.service';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { TodoList } from 'src/app/core/models/todo-list.model';
+import { TodoItem } from 'src/app/core/models/todi-item.model';
+import { Router, ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list-viewer',
@@ -6,10 +12,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-viewer.component.css']
 })
 export class ListViewerComponent implements OnInit {
+  listId$: Observable<number>;
+  list$: Observable<TodoList>;
+  items$: Observable<TodoItem[]>;
 
-  constructor() { }
+  constructor(
+    private state: StateService, 
+    private router: Router, 
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.listId$ = this.route.params.pipe(
+      map(prms => Number(prms['id']))
+    );
+
+    this.list$ = this.listId$.pipe(
+      switchMap(id => this.state.getTodoList(id))
+    );
+
+    this.items$ = this.listId$.pipe(
+      switchMap(id => this.state.getItemsInList(id))
+    )
   }
 
 }
