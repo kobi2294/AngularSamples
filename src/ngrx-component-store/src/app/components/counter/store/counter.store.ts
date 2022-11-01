817f80dac94ae248caac9f8021aef6a6090106ba
Kobi Hari <kobi2294@yahoo.com>
2022-11-01 12:45:04 +0200

    store is injectable but not provided in root

--- src/ngrx-component-store/src/app/components/counter/store/counter.store.ts
+++ src/ngrx-component-store/src/app/components/counter/store/counter.store.ts
@@ -1,6 +1,8 @@
+import { Injectable } from "@angular/core";
 import { ComponentStore } from "@ngrx/component-store";
 import { CounterState, initialCounterState } from "./counter.state";
 
+@Injectable()
 export class CounterStore extends ComponentStore<CounterState> {
     constructor() {
         super(initialCounterState);
